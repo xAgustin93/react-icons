e9530cc7309f29c6be2c1eee16ba5b505f2f81b5
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-02-25 10:44:22 +0100

    Updates

---
+++ precache-manifest.8e15dd3c5074805671d9c59719521fcc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0083d9f740503ff0761ab422fa1f9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f804d827e73b1a02b0de",
+    "url": "./static/css/2.f0193668.chunk.css"
+  },
+  {
+    "revision": "cb84561cdcfb3b4f3115",
+    "url": "./static/css/main.f2893487.chunk.css"
+  },
+  {
+    "revision": "f804d827e73b1a02b0de",
+    "url": "./static/js/2.52d6ea68.chunk.js"
+  },
+  {
+    "revision": "4668d76622c49da8967ea146d3bee6d7",
+    "url": "./static/js/2.52d6ea68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb84561cdcfb3b4f3115",
+    "url": "./static/js/main.11072220.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.11072220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ffbb08de58a4baead6",
+    "url": "./static/js/runtime-main.180a9b3c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "./static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "./static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "./static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "./static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "./static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "./static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "./static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "./static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "./static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "./static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "./static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "62f2978f27bfd52289c4a9e535160068",
+    "url": "./static/media/logo-tincode.62f2978f.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "./static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "./static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "./static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "./static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "./static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "12cbe045d9a961db01775fc11cd20728",
+    "url": "./static/media/tincode.12cbe045.ttf"
+  },
+  {
+    "revision": "55d39cba5c4da14dac96f1b296855abb",
+    "url": "./static/media/tincode.55d39cba.woff"
+  },
+  {
+    "revision": "a9edf3fd1f08119572ca7acde36e9aee",
+    "url": "./static/media/tincode.a9edf3fd.svg"
+  },
+  {
+    "revision": "ba8b9ff430532772d7556e0055c8b0c5",
+    "url": "./static/media/tincode.ba8b9ff4.eot"
+  }
+]);
